c2e0bdff34fffe13da8a85e9658c9e08a138c0f3
victorraadel <victorradael@hotmail.com>
2020-04-25 09:06:07 -0300

    :sparkles: Update Github Repositories

--- MaratonaDev3/frontend/public/scripts.js
+++
@@ -1,7 +0,0 @@
-document
-    .querySelector('header button')
-    .addEventListener('click', _ => {
-        document
-            .querySelector('.form')
-            .classList.toggle('hide')
-    })
